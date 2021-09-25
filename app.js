86c38d384eeb8e97600922ec4392617b8b213389
sngwebs786 <sngwebs786@gmail.com>
2021-09-25 20:36:08 +0500

    Work Accomplished

---
+++ app.js
@@ -0,0 +1,76 @@
+
+//write a program which shows the couter after each second.
+
+// var counter = document.getElementById('counter')
+// var count =0;
+// setInterval(function(){
+// count++;
+// counter.innerHTML = count
+// },1000)
+
+
+
+
+// write a function which takes your name and displays the greeting with your name
+
+// var name = prompt("Enter your name .. ")
+// alert("Hello"+" "+name)
+
+
+
+
+// write a function(arrow function) which takes two values and return its sum as a result
+
+//     var val1 = +prompt("Enter value 1")
+//     var val2 = +prompt("Enter value 2")
+// sum = (val1 , val2) => {
+//     return val1 + val2
+// }
+
+// var ans = sum(val1 , val2)
+// alert(ans)
+
+
+
+
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+// var num = +prompt("Enter any number")
+// mul = (n) => n * 0.5
+
+// var ans
+// ans = mul(num)
+// alert(ans)
+
+
+
+
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+
+// var arr = [1, 2, 3, 4, 5]
+// var newArr = arr.map(myFoo)
+
+// function myFoo(num) {
+//     console.log(num)
+// }
+
+
+
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+// let arr = [
+//     { id: 1, name: "abc" },
+//     { id: 2, name: "efg" },
+//     { id: 3, name: "hij" },
+//     { id: 4, name: "xyz" }
+// ]
+
+// for(var i=0;i<arr.length;i++){
+//     console.log("ID: "+arr[i].id)
+//     console.log("Name: "+arr[i].name)
+// }
